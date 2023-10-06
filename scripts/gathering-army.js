187572a40e71e0f56809228faad5a425c8a5d2cc
Staffan Pedersen <staffanbp@hotmail.com>
2023-10-06 22:03:21 +0200

    s

--- scripts/gathering-army.js
+++ scripts/gathering-army.js
@@ -3,18 +3,21 @@ import WarriorsModule from "./modules/WarriorsModule.js";
 const warriorSelection = document.querySelector("#warrior-selection");
 
 const showAllWarriors = () => {
-  const warriors = warriorsArray.getall();
+  const warriors = WarriorsModule.getall();
 
   let htmltext = "";
 
   warriors.forEach((warrior) => {
     htmltext += `
-        <article class="warrior">
+    
+        <article >
         <h3>${warrior.name}</h3>
+        <img src="images/${warrior.img}" alt="${warrior.name}" />
         <p>Price: ${warrior.price}</p>
         <p>Category: ${warrior.category}</p>
         <button class="add-to-army" data-name="${warrior.name}">Add to army</button>
-      </article>    
+      </article>   
+
     `;
   });
   warriorSelection.innerHTML = htmltext;
--- scripts/modules/WarriorsModule.js
+++ scripts/modules/WarriorsModule.js
@@ -3,57 +3,67 @@ const WarriorsModule = (() => {
     {
       name: "Snake",
       price: 200,
+      img: "warrior-1.jpg",
       category: "Warrior",
     },
     {
       name: "Giant",
       price: 500,
+      img: "warrior-2.jpg",
       category: "Warrior",
     },
     {
       name: "Big Axe",
       price: 150,
+      img: "warrior-3.jpg",
       category: "Warrior",
     },
     {
       name: "Thief",
       price: 50,
+      img: "warrior-4.jpg",
       category: "Warrior",
     },
     {
       name: "Tanks",
       price: 250,
+      img: "warrior-5.jpg",
       category: "Warrior",
     },
     {
       name: "Berserker",
       price: 275,
+      img: "warrior-6.jpg",
       category: "Warrior",
     },
     {
       name: "Horse",
       price: 100,
+      img: "horse.png",
       category: "war-machine",
     },
     {
       name: "Cannon",
       price: 300,
+      img: "cannon.png",
       category: "war-machine",
     },
     {
       name: "Catapult",
       price: 350,
+      img: "catapult.png",
       category: "war-machine",
     },
     {
       name: "Elephant",
       price: 400,
+      img: "elephant.png",
       category: "war-machine",
     },
   ];
 
   const getall = () => {
-    return warriorsArray;
+    return structuredClone(warriorsArray);
   };
 
   const getWarrior = (name) => {
