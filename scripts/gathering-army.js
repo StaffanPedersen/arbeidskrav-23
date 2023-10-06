5032e29605261a2f464467171947514058636f15
Staffan Pedersen <staffanbp@hotmail.com>
2023-10-06 13:12:08 +0200

    array + module. shopping part

--- scripts/gathering-army.js
+++ scripts/gathering-army.js
@@ -1,5 +1,25 @@
-import Warrior from './warrior.js';
+import WarriorsModule from "./modules/WarriorsModule.js";
 
+const warriorSelection = document.querySelector("#warrior-selection");
 
+const showAllWarriors = () => {
+  const warriors = warriorsArray.getall();
 
-    
+  let htmltext = "";
+
+  warriors.forEach((warrior) => {
+    htmltext += `
+        <article class="warrior">
+        <h3>${warrior.name}</h3>
+        <p>Price: ${warrior.price}</p>
+        <p>Category: ${warrior.category}</p>
+        <button class="add-to-army" data-name="${warrior.name}">Add to army</button>
+      </article>    
+    `;
+  });
+  warriorSelection.innerHTML = htmltext;
+};
+
+(() => {
+  showAllWarriors();
+})();
--- scripts/modules/WarriorsModule.js
+++ scripts/modules/WarriorsModule.js
@@ -1,31 +1,74 @@
-const WarriorModule = (() => { 
+const WarriorsModule = (() => {
+  let warriorsArray = [
+    {
+      name: "Snake",
+      price: 200,
+      category: "Warrior",
+    },
+    {
+      name: "Giant",
+      price: 500,
+      category: "Warrior",
+    },
+    {
+      name: "Big Axe",
+      price: 150,
+      category: "Warrior",
+    },
+    {
+      name: "Thief",
+      price: 50,
+      category: "Warrior",
+    },
+    {
+      name: "Tanks",
+      price: 250,
+      category: "Warrior",
+    },
+    {
+      name: "Berserker",
+      price: 275,
+      category: "Warrior",
+    },
+    {
+      name: "Horse",
+      price: 100,
+      category: "war-machine",
+    },
+    {
+      name: "Cannon",
+      price: 300,
+      category: "war-machine",
+    },
+    {
+      name: "Catapult",
+      price: 350,
+      category: "war-machine",
+    },
+    {
+      name: "Elephant",
+      price: 400,
+      category: "war-machine",
+    },
+  ];
 
+  const getall = () => {
+    return warriorsArray;
+  };
 
-    let warriors = [];
+  const getWarrior = (name) => {
+    return warriorsArray.find((warrior) => warrior.name === name);
+  };
 
-    const getWarrior = ( name ) => {
-        return warriors.find( warrior => warrior.name === name );
-    }
+  const getWarMachine = (name) => {
+    return warriorsArray.find((warMachine) => warMachine.name === name);
+  };
 
-    const addWarrior = ( name, weapon ) => {
-        warriors.push( { name, weapon } );
-    }
-
-    const removeWarrior = ( name ) => {
-        const index = warriors.findIndex( warrior => warrior.name === name );
-        warriors.splice( index, 1 );
-    }
-
-    return {
-        getWarrior,
-        addWarrior,
-        removeWarrior
-    }
-
+  return {
+    getall,
+    getWarrior,
+    getWarMachine,
+  };
 })();
 
-
-export default WarriorModule;
-
-
-
+export default WarriorsModule;
