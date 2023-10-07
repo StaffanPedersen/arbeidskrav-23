0d8dc31633f7c1e8f1ecbe3bc824ad0f8c8a2493
Staffan Pedersen <staffanbp@hotmail.com>
2023-10-07 21:38:33 +0200

    custom cursor, adjust css for cards

--- scripts/gathering-army.js
+++ scripts/gathering-army.js
@@ -15,11 +15,11 @@ const showAllWarriors = () => {
   warriors.forEach((warrior) => {
     htmltext += `
     
-        <article class="rounded-lg mx-auto shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]" >
+        <article class=" flex flex-col items-center   mx-auto rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]" >
         <h3 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${warrior.name}</h3>
-        <img src="images/${warrior.img}" alt="${warrior.name}" />
-        <p class="text-center"> Gold: ${warrior.priceGold}  </p>
-        <button class=" add-to-army focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" data-name="${warrior.name}">Add to army</button>
+        <img src="images/${warrior.img}" class="w-2/4  " alt="${warrior.name}" />
+        <p class="text-center font-bold p-2"> Gold: ${warrior.priceGold}  </p>
+        <button class="add-to-army  focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" data-name="${warrior.name}">Add to army</button>
       </article>   
 
     `;
@@ -36,14 +36,16 @@ const showAllWarriors = () => {
   warmachines.forEach((warrior) => {
     htmltext += `
 
-        <article class="rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+        <article class=" flex flex-col items-center  rounded-lg shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
         <h3 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${
           warrior.name
         }</h3>
-        <img src="images/${warrior.img}" alt="${warrior.name}" />
-        <p class="text-center"> Gold: ${warrior.priceGold} Wood: ${
-      warrior.priceWood ? ` ${warrior.priceWood}` : "0"
-    } Metal: ${warrior.priceMetal ? ` ${warrior.priceMetal}` : "0"} </p>
+        <img src="images/${warrior.img}" class="w-2/4  alt="${warrior.name}" />
+        <p class="text-center font-bold  p-2"> Gold: ${
+          warrior.priceGold
+        } Wood: ${warrior.priceWood ? ` ${warrior.priceWood}` : "0"} Metal: ${
+      warrior.priceMetal ? ` ${warrior.priceMetal}` : "0"
+    } </p>
         <button class="add-to-army focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" data-name="${
           warrior.name
         }">Add to army</button>
