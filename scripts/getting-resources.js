1e25c6072e00e87587ea8dbdad4ffd3bbdd96fb0
Staffan Pedersen <staffanbp@hotmail.com>
2023-10-10 13:21:34 +0200

    resources localstorage OK!

--- scripts/getting-resources.js
+++ scripts/getting-resources.js
@@ -1,5 +1,4 @@
-import ResoursesModule from "./modules/ResoursesModule.js";
-import LocalstorageModule from "./modules/LocalstorageModule.js";
+import ResourcesModule from "./modules/ResoursesModule.js";
 
 const resoursesSelection = document.querySelector("#resources-output");
 const gatherResourcesBtnsWood = document.querySelector(
@@ -12,77 +11,46 @@ const gatherResourcesBtnMetal = document.querySelector(
 // viser resurser på alle sidene.
 
 const showResourses = () => {
-  // const resourses = ResoursesModule.getall();
+  let goldValue = ResourcesModule.getGold();
+  let woodValue = ResourcesModule.getWood();
+  let metalValue = ResourcesModule.getMetal();
 
-  const specificValueString = LocalstorageModule.showsaveResources();
-  console.log("Specific value:", specificValueString);
+  let htmltext = "";
+
   resoursesSelection.innerHTML = `
-
         <article class="w-64 h-64   flex w-full  ">
             <div  class="flex-1 p-4 text-center flex items-center justify-center">
                 <img src="images/gold-coin.png" class="w-20 h-20  " alt="gold">
-                <p class="text-center"> Gold: ${specificValueString[0].gold} </p>
+                <p class="text-center"> Gold: ${goldValue} </p>
+      
             </div>
             <div class="flex-1 p-4 text-center flex items-center justify-center">
                 <img src="images/wood.png" class="w-20 h-20 " alt="wood">
-                <p class="text-center"> Wood:${specificValueString[0].wood} </p>
+                <p class="text-center"> Wood: ${woodValue} </p>
+           
             </div>
             <div class="flex-1 p-4  text-center flex items-center justify-center">
                 <img src="images/metal.png" class="w-20 h-20" alt="metal">
-                <p class="text-center"> Metal:${specificValueString[0].metal} </p>
+                <p class="text-center"> Metal: ${metalValue} </p>
+          
             </div>
-        </article> `;
-
-  //  resourses.forEach((resourse) => {
-  //     htmltext += `
-  //         <article class="w-64 h-64   flex w-full  ">
-  //             <div  class="flex-1 p-4 text-center flex items-center justify-center">
-  //                 <img src="images/gold-coin.png" class="w-20 h-20  " alt="gold">
-  //                 <p class="text-center"> Gold: ${resourse.gold} </p>
-  //             </div>
-  //             <div class="flex-1 p-4 text-center flex items-center justify-center">
-  //                 <img src="images/wood.png" class="w-20 h-20 " alt="wood">
-  //                 <p class="text-center"> Wood: ${resourse.wood} </p>
-  //             </div>
-  //             <div class="flex-1 p-4  text-center flex items-center justify-center">
-  //                 <img src="images/metal.png" class="w-20 h-20" alt="metal">
-  //                 <p class="text-center"> Metal: ${resourse.metal} </p>
-  //             </div>
-  //         </article> `;
-  //   });
-  // resoursesSelection.innerHTML = htmltext;
+        </article>    
+         `;
 };
 
 // funksjon for å beregne innsamlet resurser på getting-resources siden.
 
-const gatherResourcesWood = () => {
-  let randomWood = Math.floor(Math.random() * 100) + 1;
-  ResoursesModule.setWood(randomWood);
-  showResourses();
-};
-
-const gatherResourcesMetal = () => {
-  let randomMining = Math.floor(Math.random() * 100) + 1;
-
-  if (randomMining > 75) {
-    ResoursesModule.setGold(randomMining);
-  } else {
-    ResoursesModule.setMetal(randomMining);
-  }
-  showResourses();
-};
-
-// lager bilde elementer som button for å samle resurser.
+// create element button
 
 const createButtonWood = () => {
   const button = document.createElement("button");
   button.type = "button";
   button.innerHTML = `
-  <div class=" cursor-[url(/images/axe-cursor.png),_pointer]      ">
-  <img src="/images/woods-of-ghalduz.png" 
-    alt="woods-of-ghalduz" width="500" height= auto>
-    </div>
-    `;
+    <div class=" cursor-[url(/images/axe-cursor.png),_pointer]      ">
+    <img src="/images/woods-of-ghalduz.png" 
+      alt="woods-of-ghalduz" width="500" height= auto>
+      </div>
+      `;
   return button;
 };
 
@@ -90,11 +58,11 @@ const createButtonMetal = () => {
   const button = document.createElement("button");
   button.type = "button";
   button.innerHTML = `
-    <div class=" cursor-[url(/images/pickaxe-cursor.png),_pointer]     ">
-    <img src="/images/mines-of-thiartha.jpg" 
-      alt="woods-of-ghalduz" width="500" height= auto>
-      </div>
-      `;
+      <div class=" cursor-[url(/images/pickaxe-cursor.png),_pointer]     ">
+      <img src="/images/mines-of-thiartha.jpg" 
+        alt="woods-of-ghalduz" width="500" height= auto>
+        </div>
+        `;
   return button;
 };
 
@@ -102,12 +70,13 @@ gatherResourcesBtnsWood.appendChild(createButtonWood());
 gatherResourcesBtnMetal.appendChild(createButtonMetal());
 
 gatherResourcesBtnsWood.addEventListener("click", () => {
-  gatherResourcesWood(), LocalstorageModule.localstoragesave();
+  ResourcesModule.gatherWood();
+  showResourses();
 });
 gatherResourcesBtnMetal.addEventListener("click", () => {
-  gatherResourcesMetal(), LocalstorageModule.localstoragesave();
+  ResourcesModule.gatherMining();
+  showResourses();
 });
-
 // funksjon for å lagre resurser i localstorage
 
 (() => {
--- scripts/modules/ResoursesModule.js
+++ scripts/modules/ResoursesModule.js
@@ -1,45 +1,77 @@
-const ResoursesModule = (function () {
-  let resourcesArray = [];
+const ResourcesModule = (function () {
+  let resourcesArray = JSON.parse(localStorage.getItem("resources")) || [
+    {
+      gold: 0,
+      wood: 0,
+      metal: 0,
+    },
+  ];
 
   console.log(resourcesArray);
 
-  const getall = () => {
-    return resourcesArray;
+  const updateLocalStorage = () => {
+    localStorage.setItem("resources", JSON.stringify(resourcesArray));
   };
 
   const getGold = () => {
-    return structuredClone(resourcesArray).find((resourse) => resourse.gold);
+    return resourcesArray[0].gold;
   };
 
   const getWood = () => {
-    return structuredClone(resourcesArray).find((resourse) => resourse.wood);
+    return resourcesArray[0].wood;
   };
 
   const getMetal = () => {
-    return structuredClone(resourcesArray).find((resourse) => resourse.metal);
+    return resourcesArray[0].metal;
   };
 
   const setGold = (amount) => {
-    resourcesArray[0].gold += amount;
+    resourcesArray[0].gold = amount;
+    updateLocalStorage();
   };
 
   const setWood = (amount) => {
-    resourcesArray[0].wood += amount;
+    resourcesArray[0].wood = amount;
+    updateLocalStorage();
   };
 
   const setMetal = (amount) => {
-    resourcesArray[0].metal += amount;
+    resourcesArray[0].metal = amount;
+    updateLocalStorage();
+  };
+
+  const gatherMining = () => {
+    // math random for å få tilfeldig antall resurser.
+    const randomMining = Math.floor(Math.random() * 10) + 1;
+
+    if (randomMining < 7.5) {
+      setMetal(randomMining);
+    } else {
+      setGold(randomMining);
+    }
+  };
+
+  const gatherWood = () => {
+    // math random for å få tilfeldig antall resurser.
+    const randomWood = Math.floor(Math.random() * 10) + 1;
+
+    if (randomWood < 7.5) {
+      setWood(randomWood);
+    } else {
+      setGold(randomWood);
+    }
   };
 
   return {
-    getall,
     getGold,
     getWood,
     getMetal,
     setGold,
     setWood,
     setMetal,
+    gatherMining,
+    gatherWood,
   };
 })();
 
-export default ResoursesModule;
+export default ResourcesModule;
